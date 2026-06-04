9330d43cab459a2ccb006c3cc8da443a3069a97f
João Rouxinol <m54076@alunos.uevora.pt>
2026-06-04 22:09:16 +0100

    add protocol, extensions and uiview tests

--- Sources/iOSAccessibilityUtils/SwitchControl/SwitchControlCapable.swift
+++ Sources/iOSAccessibilityUtils/SwitchControl/SwitchControlCapable.swift
@@ -1,2 +1,16 @@
+import UIKit
+
+@MainActor
 public protocol SwitchControlCapable {}
 
+public extension SwitchControlCapable where Self: UIView {
+    func setAsAccessibleElement(_ isAccessible: Bool) {
+        self.isAccessibilityElement = isAccessible
+    }
+}
+
+public extension SwitchControlCapable where Self: UIViewController {
+    func configureScanningOrder(_ views: [UIView]) {
+        view.accessibilityElements = views
+    }
+}
