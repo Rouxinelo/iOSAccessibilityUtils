65a0677948fa911f5cc120ef71fd878aec4d76e0
João Rouxinol <m54076@alunos.uevora.pt>
2026-06-14 00:49:49 +0100

    remove spacing

--- Tests/iOSAccessibilityUtilsTests/DynamicFont/Tests/DynamicFontCapableUIViewTests.swift
+++ Tests/iOSAccessibilityUtilsTests/DynamicFont/Tests/DynamicFontCapableUIViewTests.swift
@@ -4,7 +4,6 @@ import UIKit
 @testable import iOSAccessibilityUtils
 
 struct DynamicFontCapableUIViewTests {
-
     @Test
     @MainActor
     func test_isAccessibilityCategory_whenCategoryIsNotAccessibility_thenMustReturnFalse() async throws {
